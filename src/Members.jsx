17c60712fb1c59afa6e2e61e484f9413e1a0d13a
adrian halvorsen <adrianhalvorsen@adrian-sin-MacBook-Pro.local>
2023-10-31 11:12:31 +0100

    member Comp oppdatert med promises/async er lame

--- src/Members.jsx
+++ src/Members.jsx
@@ -16,22 +16,19 @@ const Members = () => {
         fetchMembers();
     }, []);
 
-    const fetchMembers = async () => {
-        try {
-            const response = await axios.get('http://localhost:3006/ga/members');
-            setMembers(response.data);
-        } catch (error) {
-            console.error("Error fetching members:", error);
-        }
+    const fetchMembers = () => {
+        axios.get('/ga/members')
+            .then(response => {
+                setMembers(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching members:", error);
+            });
     };
 
-    const handleDeleteMember = async (memberNumber) => {
-        try {
-            await axios.delete(`http://localhost:3006/ga/members/${memberNumber}`);
-            fetchMembers();
-        } catch (error) {
-            console.error("Error deleting member:", error);
-        }
+    const handleDeleteMember = (memberNumber) => {
+        const updatedMembers = members.filter(m => m.memberNumber !== memberNumber);
+        setMembers(updatedMembers);
     };
 
     const handleSelectMember = (memberNumber) => {
@@ -45,32 +42,74 @@ const Members = () => {
         setMemberData(prevData => ({ ...prevData, [name]: value }));
     };
 
+    const handleUpdateMember = () => {
+        const updatedMembers = members.map(member => 
+            member.memberNumber === selectedMemberNumber 
+            ? {...member, ...memberData} 
+            : member
+        );
+        setMembers(updatedMembers);
+        setSelectedMemberNumber(null);
+    };
+
+    const handleAddMember = () => {
+        const newMember = {
+            ...memberData,
+            memberNumber: Math.max(...members.map(m => m.memberNumber)) + 1 // Gir nytt medlem neste tilgjengelige medlemsnummer
+        };
+        setMembers([...members, newMember]);
+        setMemberData({
+            firstName: '',
+            lastName: '',
+            gender: '',
+            birthYear: '',
+            email: ''
+        });
+    };
+
     return (
-        <div>
+        <div className="membersContainer">
             <h1>Medlemmer</h1>
-            <ul>
+    
+            {!selectedMemberNumber && (
+                <div className="memberForm">
+                    <h2>Legg til nytt medlem</h2>
+                    <input className="inputField" name="firstName" value={memberData.firstName} onChange={handleInputChange} placeholder="Fornavn" />
+                    <input className="inputField" name="lastName" value={memberData.lastName} onChange={handleInputChange} placeholder="Etternavn" />
+                    <input className="inputField" name="gender" value={memberData.gender} onChange={handleInputChange} placeholder="Kjønn (M/F)" maxLength="1" />
+                    <input className="inputField" name="birthYear" value={memberData.birthYear} onChange={handleInputChange} placeholder="Fødselsår" type="number" />
+                    <input className="inputField" name="email" value={memberData.email} onChange={handleInputChange} placeholder="E-post" />
+                    <button className="button" onClick={handleAddMember}>Legg til</button>
+                </div>
+            )}
+    
+            <ul className="memberList">
                 {members.map(member => (
-                    <li key={member.memberNumber}>
-                        {member.firstName} {member.lastName} ({member.email})
-                        <button onClick={() => handleSelectMember(member.memberNumber)}>Velg</button>
-                        <button onClick={() => handleDeleteMember(member.memberNumber)}>Slett</button>
+                    <li key={member.memberNumber} className="memberItem">
+                        <div className="memberDetails">
+                            {member.firstName} {member.lastName} ({member.email})
+                        </div>
+                        <div>
+                            <button className="button" onClick={() => handleSelectMember(member.memberNumber)}>Velg</button>
+                            <button className="button" onClick={() => handleDeleteMember(member.memberNumber)}>Slett</button>
+                        </div>
                     </li>
                 ))}
             </ul>
-
+    
             {selectedMemberNumber && (
-                <div>
+                <div className="memberForm">
                     <h2>Rediger medlem #{selectedMemberNumber}</h2>
-                    <input name="firstName" value={memberData.firstName} onChange={handleInputChange} placeholder="Fornavn" />
-                    <input name="lastName" value={memberData.lastName} onChange={handleInputChange} placeholder="Etternavn" />
-                    <input name="gender" value={memberData.gender} onChange={handleInputChange} placeholder="Kjønn (M/F)" maxLength="1" />
-                    <input name="birthYear" value={memberData.birthYear} onChange={handleInputChange} placeholder="Fødselsår" type="number" />
-                    <input name="email" value={memberData.email} onChange={handleInputChange} placeholder="E-post" />
-                    {/* Legg til knapper for å lagre endringer, avbryte osv. */}
+                    <input className="inputField" name="firstName" value={memberData.firstName} onChange={handleInputChange} placeholder="Fornavn" />
+                    <input className="inputField" name="lastName" value={memberData.lastName} onChange={handleInputChange} placeholder="Etternavn" />
+                    <input className="inputField" name="gender" value={memberData.gender} onChange={handleInputChange} placeholder="Kjønn (M/F)" maxLength="1" />
+                    <input className="inputField" name="birthYear" value={memberData.birthYear} onChange={handleInputChange} placeholder="Fødselsår" type="number" />
+                    <input className="inputField" name="email" value={memberData.email} onChange={handleInputChange} placeholder="E-post" />
+                    <button className="button" onClick={handleUpdateMember}>Oppdater</button>
+                    <button className="button" onClick={() => setSelectedMemberNumber(null)}>Avbryt</button>
                 </div>
             )}
         </div>
     );
-};
-
+}    
 export default Members;
